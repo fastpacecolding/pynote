a17ec9473b63587567eac3797bf9b9f538beb418
Stefan Tatschner <stefan@sevenbyte.org>
2013-12-18 18:17:16 +0100

    Bumped version number.
    Signed-off-by: Stefan Tatschner <stefan@sevenbyte.org>

--- pynote/__init__.py
+++ pynote/__init__.py
@@ -1,6 +1,6 @@
 __copyright__ = '(c) 2013 Stefan Tatschner <stefan@sevenbyte.org>'
 __license__ = 'MIT'
-__version__ = '0.1b2'
+__version__ = '0.1b3'
 
 
 # Localisation will come in later releases of pynote.
