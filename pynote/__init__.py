e43a5d0d3904ab30f14993331b0491e40792c6d7
Stefan Tatschner <stefan@sevenbyte.org>
2013-12-30 23:24:02 +0100

    Bumped year.
    Signed-off-by: Stefan Tatschner <stefan@sevenbyte.org>

--- pynote/__init__.py
+++ pynote/__init__.py
@@ -1,4 +1,4 @@
-__copyright__ = '(c) 2013 Stefan Tatschner <stefan@sevenbyte.org>'
+__copyright__ = '(c) 2013-2014 Stefan Tatschner <stefan@sevenbyte.org>'
 __license__ = 'MIT'
 __version__ = '0.1'
 
