bf5f413ba70dda9e155649ee9920279c665f9d46
Stefan Tatschner <stefan@sevenbyte.org>
2014-01-31 20:57:14 +0100

    Quick and dirty; fixes #365
    Signed-off-by: Stefan Tatschner <stefan@sevenbyte.org>

--- pynote/config.py
+++ pynote/config.py
@@ -1,32 +1,38 @@
+# REFACTOR: This module needs to be refactored!
+#   Ugly code stays here until the next minor release as a quick
+#   and dirty fix for #365.
+
 import os.path
 import configparser
 
 from pynote import helper
 
 
-config = configparser.ConfigParser()
-config.read(os.path.expanduser('~/.noterc'))
+noterc = os.path.expanduser('~/.noterc')
 
-try:
-    DATA = os.path.expanduser(config['DEFAULT']['data'])
-    DATA_FILE = os.path.join(DATA, 'data.json')
-    TRASH_FILE = os.path.join(DATA, 'trash.json')
-    REVISIONS_FILE = os.path.join(DATA, 'revisions.json')
-    DATEFORMAT = helper.expand_dateformat(config['DEFAULT']['dateformat'])
+# Set default values, see #365.
+DATA = os.path.expanduser('~/.note')
+DATA_FILE = os.path.join(DATA, 'data.json')
+TRASH_FILE = os.path.join(DATA, 'trash.json')
+REVISIONS_FILE = os.path.join(DATA, 'revisions.json')
+DATEFORMAT = '%Y-%m-%d %H:%M'
+PYGMENTS_THEME = 'default'
+
+# Check if noterc exists, see #365.
+if os.path.exists(noterc):
+    config = configparser.ConfigParser()
+    config.read()
+
+    if 'data' in config['DEFAULT']:
+        DATA = os.path.expanduser(config['DEFAULT']['data'])
+
+    if 'dateformat' in config['DEFAULT']:
+        DATEFORMAT = helper.expand_dateformat(config['DEFAULT']['dateformat'])
 
     if 'pygments_theme' in config['DEFAULT']:
         PYGMENTS_THEME = config['DEFAULT']['pygments_theme']
-    else:
-        PYGMENTS_THEME = 'default'
 
     if config['DEFAULT']['editor']:
         EDITOR = config['DEFAULT']['editor']
     elif os.getenv('EDITOR'):
         EDITOR = os.getenv('EDITOR')
-    else:
-        EDITOR = 'nano'
-
-except KeyError as e:
-    print(_("It's something wrong with your 'noterc' in {}.").format(e))
-    print(_("Try running 'note init' to create a valid 'noterc'!"))
-    exit(1)
