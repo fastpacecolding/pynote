a7dd24c3913d61dfda4aeea6b4d9e39424d07253
Stefan Tatschner <stefan@sevenbyte.org>
2014-02-05 11:54:52 +0100

    Bumped version number
    Signed-off-by: Stefan Tatschner <stefan@sevenbyte.org>

--- pynote/__init__.py
+++ pynote/__init__.py
@@ -1,6 +1,6 @@
 import gettext
 gettext.install('pynote')
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __license__ = 'MIT'
 __copyright__ = '(c) 2013-2014 Stefan Tatschner <stefan@sevenbyte.org>'
