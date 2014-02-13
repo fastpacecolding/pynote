37c6e6ff8199e1351cb3d1ac90849e10b00ec31b
Stefan Tatschner <stefan@sevenbyte.org>
2014-02-13 22:59:48 +0100

    Refactored config.py again, fixes #409
    Signed-off-by: Stefan Tatschner <stefan@sevenbyte.org>

--- pynote/config.py
+++ pynote/config.py
@@ -5,32 +5,19 @@ from pynote import helper
 
 
 NOTERC = os.path.expanduser('~/.noterc')
+config = configparser.ConfigParser()
+config.read(NOTERC)
 
-# Set default values, see #365.
-DATA = os.path.expanduser('~/.note')
-DATEFORMAT = '%Y-%m-%d %H:%M'
-PYGMENTS_THEME = 'default'
 
-# Check if noterc exists, see #365.
-if os.path.exists(NOTERC):
-    config = configparser.ConfigParser()
-    config.read(NOTERC)
+DATA = config.get('DEFAULT', 'data', fallback='~/.note')
+DATA = os.path.expanduser(DATA)
 
-    if 'data' in config['DEFAULT']:
-        DATA = os.path.expanduser(config['DEFAULT']['data'])
-
-    if 'dateformat' in config['DEFAULT']:
-        DATEFORMAT = helper.expand_dateformat(config['DEFAULT']['dateformat'])
-
-    if 'pygments_theme' in config['DEFAULT']:
-        PYGMENTS_THEME = config['DEFAULT']['pygments_theme']
-
-    if config['DEFAULT']['editor']:
-        EDITOR = config['DEFAULT']['editor']
-    elif os.getenv('EDITOR'):
-        EDITOR = os.getenv('EDITOR')
-
-# Overwrite these variables after reading the noterc!
 DATA_FILE = os.path.join(DATA, 'data.json')
 TRASH_FILE = os.path.join(DATA, 'trash.json')
 REVISIONS_FILE = os.path.join(DATA, 'revisions.json')
+
+DATEFORMAT = config.get('DEFAULT', 'dateformat', fallback='Y-m-d H:M')
+DATEFORMAT = helper.expand_dateformat(DATEFORMAT)
+
+PYGMENTS_THEME = config.get('DEFAULT', 'pygments_theme', fallback='default')
+EDITOR = config.get('DEFAULT', 'editor', fallback=os.getenv('EDITOR', 'nano'))
