fe971537487847dc7f7862209c2046c2e1c8aa2f
Stefan Tatschner <stefan@sevenbyte.org>
2014-05-31 16:15:44 +0200

    Removed manpages, version thing has to be fixed!

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 import os
 import re
 from setuptools import setup
-
-
-# https://packaging.python.org/en/latest/tutorial.html#version
-def find_version(*file_paths):
-    here = os.path.abspath(os.path.dirname(__file__))
-    version_file = open(os.path.join(here, *file_paths), 'r').read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]'",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
+import pynote
 
 with open('README.rst') as f:
     long_description = f.read()
 
 
 setup(name='pynote',
-    version=find_version('pynote', '__init__.py'),
+    # TODO: Fix version thing!
+    version=pynote.__version__,
     description='Manage notes on the commandline',
     long_description=long_description,
     author='Stefan Tatschner',
@@ -28,7 +18,6 @@ setup(name='pynote',
     license='MIT',
     packages=['pynote'],
     scripts=['note'],
-    data_files=[('share/man/man5', ['doc/man/noterc.5'])],
     install_requires=['plaintable', 'pygments'],
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Environment :: Console',
