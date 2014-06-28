f771eb042abac014ab8172a2b6d841b727ada0b4
Stefan Tatschner <stefan@sevenbyte.org>
2014-06-28 11:30:49 +0200

    Improved crypt.py

--- pynote/crypt.py
+++ pynote/crypt.py
@@ -1,29 +1,20 @@
 from Crypto import Random
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
-from base64 import b64encode
-from base64 import b64decode
 
 
-def encrypt(data, key):
+def encrypt(plaintext, key):
     iv = Random.new().read(AES.block_size)
     cipher = AES.new(key, AES.MODE_CFB, iv)
-    return cipher.encrypt(iv + data.encode())
+    return cipher.encrypt(iv + plaintext.encode())
 
 
-def decrypt(data, key):
-    iv = data[:AES.block_size]
+def decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
     cipher = AES.new(key, AES.MODE_CFB, iv)
-    return cipher.decrypt(data[AES.block_size:])
-
-
-def cipher_as_str(cipher):
-    return b64encode(cipher).decode()
-
-
-def cipher_as_bytes(cipher):
-    return b64decode(cipher).encode()
+    return cipher.decrypt(ciphertext[AES.block_size:]).decode()
 
 
 def password_digest(password):
-    return SHA256.new(password.decode()).digest()
+    password_bytes = b64encode(password.encode())
+    return SHA256.new(password_bytes).digest()
