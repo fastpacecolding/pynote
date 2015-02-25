23660c6271ba2284eb491cb3b0b14a2f2115cf7f
Stefan Tatschner <stefan@sevenbyte.org>
2015-02-25 19:56:54 +0100

    Update copyright year

--- pynote/__init__.py
+++ pynote/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '1.0.0'
 __license__ = 'MIT'
-__copyright__ = '(c) 2013-2014 Stefan Tatschner <stefan@sevenbyte.org>'
+__copyright__ = '(c) 2013-2015 Stefan Tatschner <stefan@sevenbyte.org>'
