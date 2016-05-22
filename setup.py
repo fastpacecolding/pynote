a7d1f285879d5560850c51577d0d313eefd17852
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2016-05-22 15:14:14 +0200

    Update setup.py and stuff accordingly

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
+import io
+import os.path
+import re
 from setuptools import setup
-import pynote
+
+
+# https://packaging.python.org/en/latest/single_source_version
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
 
 with open('README.rst') as f:
     long_description = f.read()
@@ -7,25 +28,20 @@ with open('README.rst') as f:
 
 setup(
     name='pynote',
-    version=pynote.__version__,
+    version=find_version('note'),
     description='Manage notes on the commandline',
     long_description=long_description,
     author='Stefan Tatschner',
-    author_email='stefan@sevenbyte.org',
+    author_email='rumpelsepp@sevenbyte.org',
     url='https://github.com/rumpelsepp/pynote',
-    license=pynote.__license__,
-    packages=['pynote'],
-    install_requires=['plaintable', 'babel', 'click', 'pyxdg'],
-    entry_points={
-        'console_scripts': [
-            'note = pynote.cli:cli',
-        ],
-    },
+    license='MIT',
+    install_requires=['plaintable', 'arrow', 'pyxdg'],
+    scripts=['note'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
